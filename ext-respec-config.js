4137c9bc96843395810124dadfa4851a3139fea3
Bernard Aboba <bernard.aboba@gmail.com>
2021-05-27 16:34:37 -0400

    Add respec configuration file

---
+++ ext-respec-config.js
@@ -0,0 +1,46 @@
+var respecConfig = {
+ "group": "webrtc",
+  github: {
+    repoURL: "https://github.com/w3c/webrtc-extensions/",
+    branch: "master"
+  },
+  "xref": ["webidl", "webrtc", "hr-time", "mediacapture-streams", "webrtc-stats"],
+  "shortName": "webrtc-extensions",
+  "specStatus": "ED",
+  "subjectPrefix": "[webrtc-extensions]"
+
+  // if there a publicly available Editor's Draft, this is the link
+  edDraftURI: "https://w3c.github.io/webrtc-extensions/",
+  editors:  [
+    //              { name: "Your Name", url: "http://example.org/",
+    //                company: "Your Company", companyURL: "http://example.com/" },
+    { name: "Bernard Aboba", company: "Microsoft Corporation",
+      w3cid: "65611"
+    }
+  ],
+  formerEditors: [
+    { name: "Henrik Boström", company: "Google", w3cid: "96936", retiredDate: "2021-04-01" }
+  ],
+  authors: [
+  ],
+  wgPublicList: "public-webrtc",
+  otherLinks: [
+    {
+      key: "Participate",
+      data: [
+        {
+          value: "Mailing list",
+          href: "https://lists.w3.org/Archives/Public/public-webrtc/"
+        }
+      ]
+    }
+  ],
+  localBiblio: {
+    "IANA-STUN-6": {
+      "title": "STUN Error Codes",
+      "href": "https://www.iana.org/assignments/stun-parameters/stun-parameters.xhtml#stun-parameters-6",
+      "publisher": "IANA"
+    }
+    }
+  }
+}
